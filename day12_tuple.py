477786a6f7d3b7995aa7379cf79b0e14cdbdf18a
Ganesh Dhatrak <ganeshjdhatrak@gmail.com>
2026-07-20 02:49:33 +0530

    Day 12 Tuples

---
+++ day12_tuple.py
@@ -0,0 +1,8 @@
+students = ("Rahul", "Ganesh", "Amit", "Priya")
+
+print(students)
+print(students[1])
+print(students[-1])
+print(students[1:3])
+print(students.count("Ganesh"))
+print(students.index("Amit"))
