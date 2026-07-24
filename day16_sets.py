85317c550709f92f7aed083f64d7d6b23c734ab0
Ganesh Dhatrak <ganeshjdhatrak@gmail.com>
2026-07-25 03:39:14 +0530

    Day 16 - Learned python sets

---
+++ day16_sets.py
@@ -0,0 +1,20 @@
+fruits = {"Apple","Banana","Mango"}
+print(fruits)
+
+numbers = {10,20,30,20,40,10,50}
+print(numbers)
+
+fruits = {"Apple","Banana","Mango"}
+fruits.add("Orange")
+print(fruits)
+
+fruits = {"Apple","Banana","Mango"}
+fruits.remove("Banana")
+print(fruits)
+
+
+fruits = {"Apple","Banana","Mango"}
+fruits.discard("Banana")
+print(fruits)
+
+
---
+++ day16_sets_practice.py
@@ -0,0 +1,9 @@
+fruits = {"Apple","Banana","Mango"}
+
+fruits.add("Orange")
+
+fruits.remove("Banana")
+
+fruits.discard("Graphes")
+
+print(fruits)
