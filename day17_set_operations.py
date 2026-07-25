be16ffc665d583b8a903ea3c6d1d4d3f4796b450
Ganesh Dhatrak <ganeshjdhatrak@gmail.com>
2026-07-26 03:59:02 +0530

    Day 17 - Learned set operations

---
+++ day17_set_operations.py
@@ -0,0 +1,20 @@
+set1 = {1,2,3}
+set2 = {4,5,6}
+print(set1.union(set2))
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+print(set1.intersection(set2))
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+print(set1.difference(set2))
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+print(set1.symmetric_difference(set2))
+
+
+
+
+        
