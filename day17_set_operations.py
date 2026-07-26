33830d9dc540b8f5f711442e088773757171ae35
Ganesh Dhatrak <ganeshjdhatrak@gmail.com>
2026-07-27 02:49:22 +0530

    Day 18 - Strings

--- day17_set_operations.py
+++ day17_set_operations.py
@@ -13,8 +13,3 @@ print(set1.difference(set2))
 set1 = {1,2,3}
 set2 = {2,3,4}
 print(set1.symmetric_difference(set2))
-
-
-
-
-        
---
+++ day18_strings.py
@@ -0,0 +1,31 @@
+name = "Ganesh"
+city = "Nashik"
+
+print(name)
+print(city)
+
+#string_indexing
+
+print(name[0])
+print(name[1])
+print(name[5])
+
+#Negative_indexing
+
+print(name[-1])
+print(name[-2])
+
+#String_slicing
+
+print(name[0:3])
+print(name[2:5])
+print(name[:4])
+print(name[3:])
+
+#common_string_method
+
+text = "python programming"
+print(text.upper())
+print(text.lower())
+print(text.replace("python","Java"))
+print(len(text))
